655ad7851782200a45fce021dcdab640a3b85e5c
Fzhut0 <80861162+Fzhut0@users.noreply.github.com>
2022-02-07 21:05:21 +0100

    Initial playercontrol script for checking player-line color

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    [SerializeField] GameObject linePrefab;
+    [SerializeField] GameObject lineControl;
+
+    GameObject lineControlObj;
+
+    Color lineColor;
+    Color playerColor;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        LineColorHandler();
+    }
+
+
+    void LineColorHandler()
+    {
+        lineColor = linePrefab.GetComponent<LineRenderer>().sharedMaterial.color;
+        playerColor = GetComponent<SpriteRenderer>().material.color;
+        lineControlObj = GameObject.FindGameObjectWithTag("LineControl");
+
+        if (lineColor != playerColor)
+        {
+            Time.timeScale = 0f;
+            lineControlObj.GetComponent<LineDrawer>().enabled = false;
+            lineControl.SetActive(false);
+            Debug.Log("wrong color");
+        }
+
+    }
+
+
+}
